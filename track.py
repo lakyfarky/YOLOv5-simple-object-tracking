a2a88646d2e5fa7059749ac756d693538876c550
Luka Farkaš <luka.farkas@gmail.com>
2023-03-07 10:44:28 +0100

    additional improvements

--- track.py
+++ track.py
@@ -2,7 +2,17 @@ import yolov5
 import cv2
 from sort import *
 import torch
-BLUE   = (255,178,50)
+
+def load_classes(classes_path="config\coco_classes.txt"):
+        classesFile = classes_path
+        classes = None
+        with open(classesFile, 'rt') as f:
+            classes = f.read().rstrip('\n').split('\n')
+        return classes
+
+
+BLUE   = (0,0,0)
+classes = load_classes()
 tracker = Sort()
 # load pretrained model
 model = yolov5.load('weights\yolov5s.pt', device="cpu")
@@ -36,16 +46,27 @@ while True:
     results = model(frame)
     detections = results.pred[0].numpy()
     tracks = tracker.update(detections)
+    # detections[:, -1] = detections[:, -1][::-1]
+    # class_ids = detections[:, -1].reshape(-1, 1)
+    # clss = class_ids.squeeze()
+    # # Concatenate class ids with the rest of the columns in `tracks`
+    # tracks = np.hstack((tracks, class_ids))
+
     for j in range (len(tracks.tolist())):
         coords = tracks.tolist()[j]
         x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+        
+        # class_id = int(clss[j])
+        # class_name = classes[class_id]
         name_idx = int(coords[4])
         name = f'ID {str(name_idx)}'
         color = BLUE
-        cv2.rectangle(frame,(x1, x2), (y1, y2), color, 2)
-        cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_COMPLEX, 0.9, color, 2)
+
+        cv2.rectangle(frame,(x1, y1), (x2, y2), color, 2)
+        cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
+        # cv2.putText(frame, class_name, (x1+50, y1-10), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 2)
         cv2.imshow('Output', frame)
-        cv2.waitKey(5)
+        cv2.waitKey(1)
     
     # # parse results
     # predictions = results.pred[0]
